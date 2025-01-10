868d7ed0ea57d33a4aa91954282fbe6403055d51
Kenzel <kenzelkdsouza@gmail.com>
2025-01-10 16:24:33 +0400

    Added Base Project

---
+++ Source/MSD_Unreal_Template/MSD_Unreal_Template.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MSD_Unreal_Template.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MSD_Unreal_Template, "MSD_Unreal_Template" );
